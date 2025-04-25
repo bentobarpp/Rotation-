6841f537f9ea03d5cecfb09a59204e953bfb7ac4
bentobarpp <bentobarpp@gmail.com>
2025-04-25 00:33:34 -0300

    coisa

--- Rotation 3/db.js
+++ Rotation 3/db.js
@@ -4,7 +4,7 @@ const mysql = require('mysql2');
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: 'giuseppe123',
+  password: 'galax246',
   database: 'rotation'
 });
 
